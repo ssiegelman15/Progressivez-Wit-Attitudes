8be2fe0f95963cae3a4339d0514d33d6fa03c976
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-06 17:55:12 -0600

    typos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("PUT to the database");
 
@@ -33,9 +32,8 @@ export const putDb = async (content) => {
   console.log("result.value", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log("GET from the database");
+  console.log("GET all from the database");
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB("jate", 1);
@@ -51,7 +49,7 @@ export const getDb = async () => {
 
   // Get confirmation of the request.
   const result = await request;
-  console.log("result.value", result);
+  console.log("result", result);
   return result;
 };
 
